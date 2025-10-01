7105bf1f353161ba623593f57b296d51c5a10e7c
Ryan Evanik <76793545+Revanik@users.noreply.github.com>
2025-09-30 21:47:34 -0400

    literally added 1 line of code

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 database_url = os.environ.get('DATABASE_URL', 'sqlite:///dividend_tracker.db')
 if database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
-    
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize extensions
