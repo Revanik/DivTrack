eae6749c4f67f949d21b275c943b6b4175f6a579
Ryan Evanik <76793545+Revanik@users.noreply.github.com>
2025-09-30 20:57:30 -0400

    Production hidden secret key

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from werkzeug.utils import secure_filename
 from models import db, User, DividendData, DividendTransaction, MonthlyTotal
 from forms import LoginForm, RegistrationForm, SettingsForm
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your-secret-key-change-this-in-production')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///dividend_tracker.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
