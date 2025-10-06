9cb611364eafba9dba1fb6ef5103e88b9e940117
Ryan Evanik <76793545+Revanik@users.noreply.github.com>
2025-10-05 22:14:57 -0400

    Changed user model to allow for longer hash

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     """User model for authentication"""
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     # Relationship to user's dividend data
